25d2847c825ae4f4f72dd5afab8a561c7c4dbfd7
Karim Gehad <44138860+MiraK1@users.noreply.github.com>
2020-06-26 06:13:58 +0100

    v1.0.6

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,38 @@
-const express = require('express');
+/** @format */
+
+const express = require("express");
 
 const router = express.Router();
 
-const {ensureAuth,ensureGuest} = require('../middleware/auth');
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-const Story = require('../models/Story');
+const Story = require("../models/Story");
 
 //? @desc login/Landing page
 // ? @route GET /
 
-router.get('/',ensureGuest,(req,res) =>{
-    console.log(req.isAuthenticated());
-    res.render('login',{
-        layout:'login'
-    });
+router.get("/", ensureGuest, (req, res) => {
+	console.log(req.isAuthenticated());
+	res.render("login", {
+		layout: "login",
+	});
 });
 
-
-//? @desc Dashboard 
+//? @desc Dashboard
 // ? @route GET /dashboard
 
-router.get('/dashboard',ensureAuth,async (req,res) =>{
-    try {
-        const stories = await Story.find({user:req.user.id}).lean();
-        console.log(stories);
-        res.render('dashboard',{
-            name: req.user.firstName,
-            stories,
-        });
-    } catch (err) {
-        console.log(err);
-        res.render('error/500');
-    }
-})
+router.get("/dashboard", ensureAuth, async (req, res) => {
+	try {
+		const stories = await Story.find({ user: req.user.id }).lean();
+		console.log(stories);
+		res.render("dashboard", {
+			name: req.user.firstName,
+			stories,
+		});
+	} catch (err) {
+		console.log(err);
+		res.render("error/500");
+	}
+});
 
 module.exports = router;
-
-
-
-
-
