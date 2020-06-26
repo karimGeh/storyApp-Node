9078bd36c0c0b4dd241f95b731185981d97850c1
Karim Gehad <44138860+MiraK1@users.noreply.github.com>
2020-06-26 06:27:56 +0100

    v1.0.7

--- app.js
+++ app.js
@@ -105,5 +105,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
 	PORT,
-	console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+	console.log(`server running in production mode on port ${PORT}`),
 );
